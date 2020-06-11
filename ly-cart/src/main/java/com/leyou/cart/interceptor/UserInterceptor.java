079683165043b6e17deaa1bcc4bade9da21cce37
DONGLI <chanxindiaolong@gmail.com>
2020-06-11 19:24:10 +0800

    完善返回状态码

--- ly-cart/src/main/java/com/leyou/cart/interceptor/UserInterceptor.java
+++ ly-cart/src/main/java/com/leyou/cart/interceptor/UserInterceptor.java
@@ -3,11 +3,12 @@ package com.leyou.cart.interceptor;
 import com.leyou.auth.entity.UserInfo;
 import com.leyou.auth.utils.JwtUtils;
 import com.leyou.cart.config.JwtProperties;
-import com.leyou.common.exception.LyException;
 import com.leyou.common.utils.CookieUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -30,6 +31,10 @@ public class UserInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String token = CookieUtils.getCookieValue(request, prop.getCookieName());
+        if(StringUtils.isBlank(token)){
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            return false;
+        }
         try {
             UserInfo user = JwtUtils.getUserInfo(prop.getPublicKey(), token);
             //spring不推荐使用
@@ -38,6 +43,7 @@ public class UserInterceptor implements HandlerInterceptor {
             return true;
         } catch (Exception e) {
             log.error("【购物车服务】解析用户身份失败。",e);
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
             return false;
         }
     }
@@ -45,5 +51,9 @@ public class UserInterceptor implements HandlerInterceptor {
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
         tl.remove();
+    }
+
+    public static UserInfo getLoginUser() {
+        return tl.get();
     }
 }
